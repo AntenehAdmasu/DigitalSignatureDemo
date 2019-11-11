b2d4ad14d0232d56dd59d33baa87974714132037
AntenehAdmasu <antenehadmasu8@gmail.com>
2019-11-11 23:54:54 +0300

    'upload'

---
+++ DigtalSignatureDemo/MainWindow.xaml.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DigtalSignatureDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static String file_content;
+        String public_key;
+        SignatureGenerator signatureGenerator;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Console.ReadLine();
+        }
+
+        private void CreateConnectionButtonHandler(object sender, RoutedEventArgs e)
+        {
+               ServerTask();       
+        }
+
+        private void ConnectButtonHandler(Object sender, RoutedEventArgs e)
+        {
+            clientTask();
+        }
+
+        public void clientTask()
+        {
+            try
+            {
+                Console.WriteLine("Client connected");
+                IPAddress ipAddr = IPAddress.Parse(ipaddress_input.Text);
+                IPEndPoint localEndPoint = new IPEndPoint(ipAddr, int.Parse(port_input.Text));
+
+                Socket senderSocket = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                
+                try
+                {
+                    senderSocket.Connect(localEndPoint);
+
+                    status_ouput.AppendText("Socket connected to -> {0} " + senderSocket.RemoteEndPoint.ToString() + "\n");
+
+                    byte[] messageSent = Encoding.ASCII.GetBytes("aomw askldjad" + "<EOF>");
+                    int byteSent = senderSocket.Send(messageSent);
+
+                    byte[] messageReceived = new byte[4096];
+
+                    int byteRecv = senderSocket.Receive(messageReceived);
+
+                    status_ouput.AppendText("Message from Server -> {0}" + Encoding.ASCII.GetString(messageReceived, 0, byteRecv) + "\n");
+
+                    senderSocket.Shutdown(SocketShutdown.Both);
+                    senderSocket.Close();
+
+
+                }
+                catch (ArgumentNullException ane)
+                {
+                    MessageBox.Show($"ArgumentNullException : {0}", ane.ToString());
+                }
+                catch (SocketException se)
+                {
+                    MessageBox.Show($"SocketException : {0}", se.ToString());
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show($"Unexpected exception : {0}", e.ToString());
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+
+        }
+
+        public async void ServerTask()
+        {
+            Console.WriteLine("Server started");
+            IPAddress[] ipHost = Dns.GetHostAddresses(Dns.GetHostName());
+            IPAddress ip = null;
+            foreach (IPAddress ipAddr in ipHost)
+            {
+
+                if (ipAddr.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    ip = ipAddr;
+                    Console.WriteLine(ip);
+
+                    ipaddress_input.Text = ipAddr.ToString();
+                }
+            }
+
+            Console.WriteLine("After foreach loop");
+            IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Parse(ipaddress_input.Text), int.Parse(port_input.Text));
+            status_ouput.AppendText("Shell >> Ip Address: " + ipaddress_input.Text.ToString() + ":" + port_input.Text.ToString() + "\n");
+            
+            
+            Socket listener = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(10);
+
+                while (true)
+                {
+                    SocketAsyncEventArgs el = new SocketAsyncEventArgs();
+                    el.Completed += test;
+
+                    Socket clientSocket = listener.AcceptAsync();
+
+                    SignatureGenerator sg = new SignatureGenerator();
+                    sg.HashFile(file_content);
+                    byte[] bytes = new Byte[4096];
+                    string data = null;
+                    signatureGenerator = new SignatureGenerator();
+                    while (true)
+                    {
+                        int numByte = clientSocket.Receive(bytes);
+                        data += Encoding.ASCII.GetString(bytes, 0, numByte);
+                        Console.WriteLine("data: " + data);
+
+                        if (data.IndexOf("<EOF>") > -1)
+                            break;
+                    }
+
+                    status_ouput.AppendText("Shell >> Text received -> {0} " + data + "\n");
+                    byte[] message = Encoding.ASCII.GetBytes("Anzzi <EOF>");
+                    signatureGenerator.HashFile(file_content);
+
+
+                    //---------------------------- T O   B E   T E S T E D   ---------------------------------
+
+                    byte[] message2 = Encoding.ASCII.GetBytes(file_content);
+
+
+
+
+
+                    //---------------------------- E N D  O F  F I L E ---------------------------------------
+
+                    
+
+
+                    clientSocket.Send(message);
+
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+                }
+            }
+
+
+
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.ReadLine();
+        }
+
+        public void test(object sender, SocketAsyncEventArgs socketAsyncEventArgs)
+        {
+            Console.WriteLine("Testerajbajhjabhj");
+        }
+        private void ReadFile()
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            bool? result = dlg.ShowDialog();
+
+            string filename = null;
+            if (result == true)
+            {
+                filename = dlg.FileName;
+                selected_file.Text = System.IO.Path.GetFileName(filename);
+            }
+
+            using (var fileStream = new FileStream(filename, FileMode.Open, FileAccess.Read))
+            {
+                var sr = new StreamReader(fileStream, Encoding.UTF8);
+                file_content = sr.ReadToEnd();
+                status_ouput.Text = "";
+                status_ouput.Text = file_content;
+                selected_file.FontSize = 16;
+            }
+
+
+        }
+
+        private void BrowseFileButtonHandler(object sender, RoutedEventArgs e)
+        {
+            ReadFile();
+        }
+
+        //public void SendButtonHandler(Object sender, RoutedEventArgs e)
+        //{
+        //    String hashedFile = signatureGenerator.HashFile(file_content);
+            
+        //    if(MainWindow.file_content == ""){
+        //        selected_file.Text = "Please choose the file first";
+        //    }
+        //    else{
+        //        String content_to_hash = file_content; 
+        //    }
+        //}
+
+    }
+}
---
+++ DigtalSignatureDemo/SignatureGenerator.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Org.BouncyCastle.Asn1.Pkcs;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Generators;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.OpenSsl;
+using Org.BouncyCastle.Security;
+
+namespace DigtalSignatureDemo
+{
+    class SignatureGenerator
+    {
+        private RsaKeyParameters privateKey;
+        private RsaKeyParameters publicKey;
+        
+
+        TextWriter textWriter;
+        PemWriter pemWriter;
+
+        public SignatureGenerator()
+        {
+
+            RsaKeyPairGenerator rsaKeyPairGen = new RsaKeyPairGenerator();
+            rsaKeyPairGen.Init(new KeyGenerationParameters(new SecureRandom(), 2048));
+            AsymmetricCipherKeyPair keyPair = rsaKeyPairGen.GenerateKeyPair();
+
+            privateKey = (RsaKeyParameters)keyPair.Private;
+            publicKey = (RsaKeyParameters)keyPair.Public;                 
+
+        }
+        public string getPublicKeyString()
+        {
+            String stringpublicKey;
+
+            textWriter = new StringWriter();
+            pemWriter = new PemWriter(textWriter);
+            pemWriter.Writer.Flush();
+            pemWriter.WriteObject(publicKey);
+            stringpublicKey = textWriter.ToString();
+
+            return stringpublicKey;
+        }
+
+        public string getPrivateKeyString()
+        {
+            String stringprivateKey;
+
+            textWriter = new StringWriter();
+            pemWriter = new PemWriter(textWriter);
+            pemWriter.Writer.Flush();
+            pemWriter.WriteObject(publicKey);
+            stringprivateKey = textWriter.ToString();
+
+            return stringprivateKey;
+        }
+
+        public string HashFile(String file_content)
+        {
+            ISigner sign = SignerUtilities.GetSigner(PkcsObjectIdentifiers.Sha1WithRsaEncryption.Id);
+            sign.Init(true, privateKey);
+            byte[] fileInBytes = ASCIIEncoding.ASCII.GetBytes(file_content);
+            sign.BlockUpdate(fileInBytes, 0, fileInBytes.Length);
+            byte[] signature = sign.GenerateSignature();
+            string fileHash = ByteArrayToString(signature);
+            return fileHash;
+        }
+        public bool verifySignature(byte[] file_content, byte[] signature)
+        {
+            ISigner sign1 = SignerUtilities.GetSigner(PkcsObjectIdentifiers.Sha1WithRsaEncryption.Id);
+            sign1.Init(false, publicKey);
+            sign1.BlockUpdate(file_content, 0, file_content.Length);
+            bool status = sign1.VerifySignature(signature);
+
+            Console.WriteLine();
+            Console.Write("Status of the digital signature : " + " ");
+
+            return status;
+
+        }
+        
+
+        private static string ByteArrayToString(byte[] byteArray)
+        {
+            StringBuilder mystring = new StringBuilder(byteArray.Length);
+            for (int i = 0; i < byteArray.Length; i++)
+            {
+                mystring.Append(byteArray[i].ToString("X").ToLower());
+            }
+            return mystring.ToString();
+        }
+
+    }
+}
